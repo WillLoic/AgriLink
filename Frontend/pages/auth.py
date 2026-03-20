ca1282b413d0c1065e0ef590dfc2761968a89672
WillLoic <willloic36@gmail.com>
2026-03-20 15:46:11 +0100

    Update auth.py

--- Frontend/pages/auth.py
+++ Frontend/pages/auth.py
@@ -8,6 +8,18 @@ import os
 
 
 # --- CONFIGURATION DE LA PAGE ---
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            .stAppDeployButton {display:none;}
+            [data-testid="stToolbar"] {display:none;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 st.set_page_config(
     page_title="AgriLink - Analyse Agricole IA",
     page_icon="🌱",
