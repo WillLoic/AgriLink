7e636c2e4c1df3c22cdc450209b42224ba991872
WillLoic <willloic36@gmail.com>
2026-03-20 15:46:30 +0100

    Update forgot_password.py

--- Frontend/pages/forgot_password.py
+++ Frontend/pages/forgot_password.py
@@ -4,6 +4,18 @@ from datetime import datetime
 import os
 
 # --- CONFIGURATION ---
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            .stAppDeployButton {display:none;}
+            [data-testid="stToolbar"] {display:none;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 st.set_page_config(page_title="Mot de passe oublié - AgriLink", layout="centered")
 
 # --- CSS PERSONNALISÉ ---
