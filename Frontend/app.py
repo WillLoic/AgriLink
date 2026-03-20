f9ceb9bb0e28315ac198e79e1d41513acbf76109
WillLoic <willloic36@gmail.com>
2026-03-20 15:40:05 +0100

    masquer les tools bar de streamlit

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,5 +1,16 @@
 import streamlit as st
 
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            .stAppDeployButton {display:none;}
+            [data-testid="stToolbar"] {display:none;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 # --- REDIRECTION VERS LANDING PAGE ---
 # Ce fichier sert de point d'entrée principal
 # Il redirige automatiquement vers la landing page
