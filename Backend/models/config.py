0c5054efe6bf9454a8e69e5057449be47db0149e
WillLoic <willloic36@gmail.com>
2026-03-20 08:52:45 +0100

    Update config.py

--- Backend/models/config.py
+++ Backend/models/config.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-#from app import app
+from app import app
 from flask_marshmallow import Marshmallow
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, text, func
