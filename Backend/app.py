7fb496fd6152aa6a5726ef197dd22021aab0257a
WillLoic <willloic36@gmail.com>
2026-03-21 18:34:02 +0100

    debug des variables environnement

--- Backend/app.py
+++ Backend/app.py
@@ -28,7 +28,7 @@ app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL', 'False').lower() == 'true
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER', 'noreply@agri-link.com')
-
+print(app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])  # Debug : vérifier les variables d'environnement
 mail = Mail(app)
 
 # Configuration simple de Swagger
